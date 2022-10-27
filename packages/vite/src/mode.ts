1a186a9a4ce4d6c2695115c990cfeff2ca9bbbe9
沈赟杰 <shenyunjie@znrank.com>
2022-10-27 21:56:16 +0800

    update

--- packages/vite/src/mode.ts
+++ packages/vite/src/mode.ts
@@ -4,4 +4,5 @@ export const SSR = process.env.SSR === 'true';
 export const KIND = process.env.KIND as 'spa' | 'client' | 'server';
 
 export const OUTPUT_SPA_DICTIONARY = resolve(process.cwd(), 'dist', 'spa');
-export const OUTPUT_SSR_DICTIONARY = resolve(process.cwd(), 'dist', 'ssr');
+export const OUTPUT_SSR_CLIENT_DICTIONARY = resolve(process.cwd(), 'dist', 'ssr', 'client');
+export const OUTPUT_SSR_SERVER_DICTIONARY = resolve(process.cwd(), 'dist', 'ssr', 'server');
--- packages/vite/src/server.build.ts
+++ packages/vite/src/server.build.ts
@@ -1,11 +1,11 @@
 import type { Plugin } from 'vite';
-import { OUTPUT_SSR_DICTIONARY } from './mode';
+import { OUTPUT_SSR_SERVER_DICTIONARY } from './mode';
 import { resolve } from 'path';
 export function createServerBuilder(file: string): Plugin {
   return {
     name: 'codix:build:ssr:server',
     config(configs) {
-      configs.build.outDir = OUTPUT_SSR_DICTIONARY;
+      configs.build.outDir = OUTPUT_SSR_SERVER_DICTIONARY;
       configs.build.ssr = resolve(process.cwd(), file);
       if (configs.build.rollupOptions?.manualChunks) {
         delete configs.build.rollupOptions.manualChunks;
